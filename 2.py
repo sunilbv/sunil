e4b8bd7cab72de5320c3e1bf262464b9eb21e768
sunilbv <bsunil027@gmail.com>
2017-07-15 15:29:12 +0530

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,12 @@
+# sunil
+num = int(input("Enter a number: "))
+i=num/2
+if(num%i==0):
+#print 'As u all know 2,3 and 5 are prime numbers so dont try that'
+
+  
+
+  print 'is a not prime number',num
+else:
+  print"is  a prime number",num
+       
